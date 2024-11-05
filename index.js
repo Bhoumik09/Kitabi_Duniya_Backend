d147958d4db7d8d3210af0b5d723baa3fc8a0891
Bhoumik <Bhoumikchopra12@gmail.com>
2024-11-05 11:19:03 +0530

    cors checked

--- index.js
+++ index.js
@@ -13,12 +13,7 @@ const corsOptions = {
 };
 
 // Example Node.js/Express code
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+
 server.use(cors(corsOptions));
 server.use(express.json());
 
